82d4508fdcac232e26cca6ee66b41be80adf5456
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-23 19:40:17 -0400

    Added a login mutation that authinticate a current user based on their email and password and calls the isCorrectPassword function that is in the models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,8 +20,35 @@ const resolvers = {
   Mutation: {
     // Creates a single user and creates a jwt token for that user
     addUser: async (parent, args) => {
+      // First we create the user
       const user = await User.create(args);
 
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+
+    // A login mutation finds a specific user by email in the db
+    login: async (parent, { email, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
       const token = signToken(user);
 
       return { token, user };
