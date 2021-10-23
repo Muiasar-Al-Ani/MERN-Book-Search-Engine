3813797a9e9edd2087efdba5fe2b981e1c481a0c
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-23 19:56:35 -0400

    Added a removeBook mutation that checks if the user logged in then it allows the user to delete a book from the savedBooks in the db

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -72,6 +72,23 @@ const resolvers = {
       // If user attempts to execute this mutation and isn't logged in, throw an error
       throw new AuthenticationError("You need to be logged in!");
     },
+
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    removeBook: async (parent, { bookId }, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+
+          // Delete the book based on the book ID from the db
+          { $pull: { savedBooks: { bookId: bookId } } },
+
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+    },
   },
 };
 
