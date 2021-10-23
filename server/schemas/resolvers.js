6a0e77a96e43124cee9396e58ac5c088eee8564f
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-23 19:06:33 -0400

    Added the query needed dependencies and a query for a single user

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    singleUser: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+};
+
+module.exports = resolvers;
