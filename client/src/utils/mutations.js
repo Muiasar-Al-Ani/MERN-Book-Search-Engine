ba9608eaf8f23577d6d837c785ed31a115d74c49
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-23 21:09:22 -0400

    Added the login mutation query to be executed by the apollo client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,22 @@
 import gql from "graphql-tag";
+
 // Create a GraphQL mutation query to be executed by Apollo Client and adds a new user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Create a GraphQL mutation login query to be executed by Apollo Client and
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
